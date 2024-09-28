f6ae7483c45116abff7cc8c8427c5687a44d2ce3
Celso Aquino <cvaquino03@outlook.com>
2024-09-29 00:15:10 +0800

    Updated skills and JS

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// script.js
-const burgerMenu = document.getElementById('burger-menu');
-const navLinks = document.getElementById('nav-links');
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navLinks = document.querySelector('.nav-links');
 
-burgerMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    burgerMenu.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
 });
